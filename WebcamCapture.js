941fbfb78835c2865b35e93ab99d3d5c85e14500
kilianhoppner <162424595+kilianhoppner@users.noreply.github.com>
2024-04-17 07:32:09 +1000

    Update WebcamCapture.js

--- WebcamCapture.js
+++ WebcamCapture.js
@@ -5,24 +5,21 @@ let cam;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cam = createCapture(VIDEO);
+  cam.size(windowWidth, windowHeight); // set capture size to window size
   cam.hide();
   frameRate(3);
 }
 
 function draw() {
   background(255);
-  //move image by the width of image to the left
-  translate(cam.width, 0);
-  //then scale it by -1 in the x-axis
-  //to flip the image
+  // Mirror the video horizontally
+  translate(width, 0);
   scale(-1, 1);
-  //draw video capture feed as image inside p5 canvas
-  image(cam, windowWidth/-1.8, 0, windowWidth, windowHeight);
-  
+  // Draw video capture feed as image inside p5 canvas
+  image(cam, 0, 0, width, height);
 }
 
-function mousePressed(){
-     
+function mousePressed() {
   save('selfie.png');
   return false;
 }
